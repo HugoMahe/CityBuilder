b63f927acea1c9f7a1ac61b236b8d6d9f2888c14
Lazare29 <hugo29000@hotmail.fr>
2020-11-07 19:05:04 +0100

    EMPTY

--- GUI/menuBatiment.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-func update_info(batiment):
-	$VBoxContainer/Nom.text = batiment.nom
-	$VBoxContainer/Ressource/Type.text = batiment.typeRessource
-	$VBoxContainer/Ressource/Valeur.text = String(batiment.prod)
